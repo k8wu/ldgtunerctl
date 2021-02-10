8638d22d10703b925e269ba46e6afb5d18af1007
k8wu <18063294+k8wu@users.noreply.github.com>
2021-02-09 20:22:55 -0500

    Making the CommLink buffer clear between read/write

--- src/classes/Tuner.class.cxx
+++ src/classes/Tuner.class.cxx
@@ -72,6 +72,7 @@ bool Tuner::commSync() {
     const char *cmdChar = "Z";
     std::string receivedSyncString = rawCommand(cmdChar, true);
     if(receivedSyncString.length() > 0 && receivedSyncString.compare("000000000000000AzAz") == 0) {
+      masterCommLink->readFromDevice();
       return true;
     }
   }
